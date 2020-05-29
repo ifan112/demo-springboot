c9525f6fc124aca71bafac0ef895e9d7cd0733dd
ifan112 <ifan.w112@gmail.com>
2020-05-29 19:05:18 +0800

    新增测试应用上下文的属性内容来源

--- demo-springboot-t/src/test/java/com/ifan112/demo/springboot/test/DemoTest.java
+++ demo-springboot-t/src/test/java/com/ifan112/demo/springboot/test/DemoTest.java
@@ -25,6 +25,9 @@ import org.springframework.test.context.junit4.SpringRunner;
         properties = {
                 "url.baidu=https://www.baidu.cn",    // 行内声明的属性
                 "spring.main.banner-mode=off"
+        },
+        locations = {
+                "demo-test.properties"               // 行内声明的属性文件，要求文件与测试类在同一个包下
         }
 )
 @IfProfileValue(name = "user.name", value = "ifan") // 当系统用户是 ifan 时才会执行测试用例
@@ -37,6 +40,9 @@ public class DemoTest {
 
     @Value("${url.baidu}")
     private String urlBaidu;
+
+    @Value("${url.tmall}")
+    private String urlTMall;
 
 
     /**
@@ -55,12 +61,30 @@ public class DemoTest {
     @Test
     public void test() {
         /*
+         * 注意，此时，测试应用上下文的基于属性的配置项来源包括：
+         *
+         * 1. 系统属性
+         * 2. 系统环境变量
+         * 3. src/main/resources/application.properties 属性文件
+         * 4. src/test/resources/application.properties 属性文件，
+         *    如果同时存在 3 和 4 中的属性文件，那么将只会使用 4 中的属性文件
+         * 5. src/test/resources/com/ifan112/demo/springboot/test/demo-test.properties
+         * 6. 当前测试类上基于 @TestPropertySource 声明的行内属性
+         */
+
+        /*
          * 配置项 url.baidu 的来源一共有三处，按照优先级从低到高分别是：
          *
          * 1. src/main/resources/application.properties 属性文件
-         * 2. src/test/resources/application.properties 属性文件
+         * 2. src/test/resources/com/ifan112/demo/springboot/test/demo-test.properties 属性文件
          * 3. 当前测试类上基于 @TestPropertySource 声明的行内属性
          */
         Assert.assertEquals("https://www.baidu.cn", urlBaidu);
+
+        /*
+         * 配置项 urlTMall 的来源只有一处，通过 @TextPropertySource#locations 方法声明的属性文件。
+         * 它的完整路径是：src/test/resources/com/ifan112/demo/springboot/test/demo-test.properties。
+         */
+        Assert.assertEquals("https://www.tmall.com", urlTMall);
     }
 }

a83832c9bcad6c793f5b9391ee6f4888993932a5
ifan112 <ifan.w112@gmail.com>
2020-05-31 23:42:50 +0800

    修复测试用例

--- demo-springboot-t/src/test/java/com/ifan112/demo/springboot/test/controller/EEurekaControllerTest.java
+++ demo-springboot-t/src/test/java/com/ifan112/demo/springboot/test/controller/EEurekaControllerTest.java
@@ -10,11 +10,15 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -35,26 +39,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * 但是，对于需要通过 @SpyBan 方式注入的非 Controller 层组件，如果它需要其它组件，那么将会由于缺少可用的其它组件实例
  * 而导致该 Bean 无法被成功创建出来。
  *
- * 此时，解决的方式有两种：
- * 1. 在测试类中使用 @MockBean 提供可用的第三方组件实例，但是这又需要同时对 Mock 出来的第三方组件实例进行定制。
- * 2. 在测试类上使用 @ContextConfiguration 指定上下文，让 ApplicationContext 管理第三方组件。
+ * 此时，解决的方式是：
+ * 在测试类中使用 @MockBean 提供可用的第三方组件实例，但是这又需要同时对 Mock 出来的第三方组件实例进行定制。
  *
+ *
+ * 遇到一个奇怪的问题，如果当前测试类以'数字'结尾，那么 mvn test 命令将不会执行这个类中的测试用例，不清楚原因。
  */
 
-@WebMvcTest
-@RunWith(SpringRunner.class)
-@ContextConfiguration(
-        classes = {
-                /*
-                 * 由 @WebMvcTest 声明的测试上下文，默认不会管理有 @Component 注解的组件，
-                 * 这样通常会导致在使用 @SpyBean 管理组件时的无法注入它所依赖的其它组件
-                 * 因此，这里通过指定测试上下文，让其管理所有组件
-                 */
-                DemoSpringBootTestApp.class,
-                DemoSpringBootTestConfig.class
-        }
+@WebMvcTest(
+        controllers = EurekaController.class    // 指定待测试的 Controller，避免管理其它 Controller 及其依赖，
+                                                // 否则可能导致由于依赖不满足而启动失败。例如依赖 @Service 或 @Repository 组件
+                                                // 而这些组件由于在 WebMvcTest 上下文中根本不进行管理，因此无法实现自动注入。
 )
-public class EurekaControllerTest2 extends EurekaBaseTest {
+@RunWith(SpringRunner.class)
+public class EEurekaControllerTest extends EurekaBaseTest {
 
     /**
      * 使用 @SpyBean 管理的 EurekaService 组件，只会代理在测试用例指定方法，
@@ -62,6 +60,16 @@ public class EurekaControllerTest2 extends EurekaBaseTest {
      */
     @SpyBean
     private EurekaService eurekaService;
+
+    /**
+     * 由于 @WebMvcTest 将会使测试应用上下文不会管理非 Controller 层的组件（如 DemoSpringBootTestConfig 这个配置组件
+     * 以及在其内部通过 @Bean 声明的 RestTemplate 组件），因此这里通过 @MockBean 显式声明一个以自动注入到 EurekaService 组件中。
+     *
+     * 如果 EurekaController 依赖的许多的组件，而这些组件又依赖了其它的组件，那么这些组件可能都需要通过 @MockBean 的方式显式声明。
+     * 这也算是 @WebMvcTest 带来的一个不好的影响。
+     */
+    @MockBean
+    private RestTemplate restTemplate;
 
     @Autowired
     private MockMvc mockMvc;
